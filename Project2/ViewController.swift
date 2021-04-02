f4d11951d65e910442fc6d256cfd746397e41bb9
Edson <edson_neto1996@hotmail.com>
2021-04-02 14:46:11 -0300

    Initial Commit

---
+++ Project2/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Edson Neto on 02/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var button1: UIButton!
+    @IBOutlet var button2: UIButton!
+    @IBOutlet var button3: UIButton!
+    
+    var countries = [String]()
+    var score = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
+        
+        button1.layer.borderColor = UIColor.lightGray.cgColor //UIColor retorna um UIColor, mas o borderColor não sabe o que é pois está em uma CoreAnimation layer, por isso o .cgColor para converter automaticamente
+//        button2.layer.borderColor = UIColor(red: 1.0, green: 0.6, blue: 0.2, alpha: 1.0).cgColor
+        button2.layer.borderColor = UIColor.lightGray.cgColor
+        button3.layer.borderColor = UIColor.lightGray.cgColor
+
+        askQuestion()
+
+    }
+
+    func askQuestion(){
+        button1.setImage(UIImage(named: countries[0]), for: .normal) //setImage atribui uma UIImage ao botão,o for: diz qual estado deve mudar, o normal é o estado padrão
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
+    }
+
+}
+
